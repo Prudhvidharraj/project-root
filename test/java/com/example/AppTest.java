8be0e56d36f80a53c1a0df485c7596fb9a43e91a
Prudhvidharraj <jadiprudhvidharraj@gmail.com>
2025-02-03 08:35:40 +0530

    first commit

--- test/java/com/example/AppTest.java
+++ test/java/com/example/AppTest.java
@@ -1,11 +1,23 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import spark.Spark;
 
 public class AppTest {
+
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    public void testHelloRoute() {
+        // Start the server before tests
+        Spark.awaitInitialization();
+        
+        // Make a request to the /hello route
+        String response = TestUtil.getResponse("http://localhost:8080/hello");
+        
+        // Assert that the response is what we expect
+        assertEquals("Hello from CI/CD Pipeline!", response);
+        
+        // Stop the server after tests
+        Spark.stop();
     }
 }
